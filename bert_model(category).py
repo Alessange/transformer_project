e8ddc49cad0727d5ac181ac75c0181b8829763ba
Alessange <56106326+Alessange@users.noreply.github.com>
2023-06-29 20:28:43 +0800

    Update bert_model(category).py

--- bert_model(category).py
+++ bert_model(category).py
@@ -4,7 +4,7 @@ from datasets import load_dataset
 from sklearn.preprocessing import LabelEncoder
 
 
-class Dataset(torch.utils.data.Dataset):
+class Dataset(torch.utils.data.Dataset): #default setting for dataset
     def __init__(self, split):
         self.dataset = load_dataset("json",
                                     data_files='/Users/huangshixun/Desktop/Transformer/output.json',
@@ -24,7 +24,7 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 dataset = Dataset('train')
 
 
-def collate_fn(data):
+def collate_fn(data):# method for dealing data
     contents, categories = zip(*data)
 
     data = tokenizer.batch_encode_plus(batch_text_or_text_pairs=contents,
@@ -53,7 +53,7 @@ for param in pretrained.parameters():
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc = torch.nn.Linear(768, len(set(dataset.dataset['category'])))
+        self.fc = torch.nn.Linear(768, len(set(dataset.dataset['category']))) #the regression model depends on the number of text classifications you want to implement
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         with torch.no_grad():
@@ -67,7 +67,7 @@ class Model(torch.nn.Module):
 
 model = Model()
 
-optimizer = AdamW(model.parameters(), lr=5e-4)
+optimizer = AdamW(model.parameters(), lr=5e-4) #model set
 criterion = torch.nn.CrossEntropyLoss()
 
 model.train()
@@ -80,10 +80,10 @@ for i, (input_ids, attention_mask, token_type_ids, categories) in enumerate(
     optimizer.step()
     optimizer.zero_grad()
 
-    if i % 15 == 0:
+    if i % 15 == 0: #display result of loss info and accuracy each 15 steps
         out = out.argmax(dim=-1)
         accuracy = (out == categories).sum().item() / len(categories)
         print(f"Step: {i}, Loss: {loss.item()}, Accuracy: {accuracy}")
 
-    if i == 1000:
-        break
+    if i == 1000: # stop running when the 1000steps reach
+        break 
